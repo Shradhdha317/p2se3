3d4e74e971aa600c14c7e0e33644f08b603b0bdb
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-12 12:09:23 -0400

    try this way

--- data.php
+++ data.php
@@ -1,13 +1,23 @@
+		
 <?php
 
-	function get_price($product)
-	{
-		$conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/sql.sql", NULL, NULL); 
-	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
+function get_price($product)
+{
+        
+	$servername = "patels18.mysql.database.azure.com";
+	$username = "patels18@patels18";
+	$password = "Krishna@0115";
+	$dbname = "patels18_db";
 
-		
-		$sql = "SELECT price FROM products WHERE product = '$product'";
+       
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) 
+	{
+            die("Connection failed: " .$conn->connect_error);
+       	}
+
+       $sql = "SELECT price FROM products WHERE product = '$product'";
 
        $result = $conn->query($sql);
 	
@@ -27,13 +37,23 @@ else {
     return $price;
 }
 
-	function get_designer($product)
-	{
-		$conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
-	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
+function get_designer($product)
+{
+        
+	$servername = "patels18.mysql.database.azure.com";
+	$username = "patels18@patels18";
+	$password = "Krishna@0115";
+	$dbname = "patels18_db";
 
-		$sql1 = "SELECT designer FROM products WHERE product = '$product'";
+       
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) 
+	{
+            die("Connection failed: " .$conn->connect_error);
+       	}
+
+     	$sql1 = "SELECT designer FROM products WHERE product = '$product'";
 
        	$result1 = $conn->query($sql1);
 
