7256c6440c42dfe2f21008ed9d6c59e2def7a99c
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-12 13:45:44 -0400

    trying to solve connection error

--- data.php
+++ data.php
@@ -4,18 +4,10 @@
 function get_price($product)
 {
         
-	$servername = "patels18.mysql.database.azure.com";
-	$username = "patels18@patels18";
-	$password = "Krishna@0115";
-	$dbname = "patels18_db";
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-	mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-        
-        if ($conn->connect_error) 
-	{
-            die("Connection failed: " .$conn->connect_error);
-       	}
+        $conn=mysqli_init(); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306); 
+ 
 
        $sql = "SELECT price FROM products WHERE product = '$product'";
 
@@ -40,19 +32,10 @@ else {
 function get_designer($product)
 {
         
-	$servername = "patels18.mysql.database.azure.com";
-	$username = "patels18@patels18";
-	$password = "Krishna@0115";
-	$dbname = "patels18_db";
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-	mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+	$conn=mysqli_init(); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306); 
         
-        if ($conn->connect_error) 
-	{
-            die("Connection failed: " .$conn->connect_error);
-       	}
-
      	$sql1 = "SELECT designer FROM products WHERE product = '$product'";
 
        	$result1 = $conn->query($sql1);
