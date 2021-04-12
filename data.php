75d9ab2ec569df95652fc789c00b668c2d86a334
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-12 18:43:10 -0400

    try this

--- data.php
+++ data.php
@@ -5,8 +5,8 @@ function get_price($product)
 {
         
         $conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
-	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18", 3306); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
+	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306); 
  
 
        $sql = "SELECT price FROM products WHERE product = '$product'";
@@ -32,7 +32,7 @@ function get_designer($product)
 {
         
 	$conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
 	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306); 
         
      	$sql1 = "SELECT designer FROM products WHERE product = '$product'";
