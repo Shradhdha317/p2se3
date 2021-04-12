6a14a7f3a7f00c283eec8002b388ccf0de18f05b
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-12 09:36:43 -0400

    some changes in code

--- data.php
+++ data.php
@@ -7,29 +7,51 @@
 	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
 
 		
-		$sql = ("SELECT price FROM products WHERE product = '$product'");
-		$stmt = $conn->query("$sql");
-		$row = $stmt->fetch();
-		$price = $row["price"];
-		$conn = NULL;
-	 	return $price;	
-		
-	}
+		$sql = "SELECT price FROM products WHERE product = '$product'";
+
+       $result = $conn->query($sql);
 	
+
+         if ($result->num_rows > 0) {
+             
+             while($row = $result->fetch_assoc()) {
+                      $price = $row["price"];
+      }
+    } 
+else {
+                     $price = null;
+        }
+
+    $conn->close();
+
+    return $price;
+}
+
 	function get_designer($product)
 	{
 		$conn=mysqli_init(); 
 	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
 	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
 
-		$sql = ("SELECT designer FROM products WHERE product = '$product'");
-		$stmt = $conn->query("$sql");
-		$row = $stmt->fetch();
-		$designer = $row["designer"];
-		$conn = NULL;
-	 	return $designer;	
-		
-	}
+		$sql1 = "SELECT designer FROM products WHERE product = '$product'";
+
+       	$result1 = $conn->query($sql1);
+
+         if ($result1->num_rows > 0) {
+             
+             while($row = $result1->fetch_assoc()) {
+                      $designer = $row["designer"];
+      }
+    } 
+else {
+                     $designer = null;
+        }
+
+    $conn->close();
+
+    return $designer;
+}
+
 
 
 ?>
