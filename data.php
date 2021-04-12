4470b7f27522c4a27618ee3d59440483be19e35c
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-12 10:49:59 -0400

    just a shot

--- data.php
+++ data.php
@@ -3,7 +3,7 @@
 	function get_price($product)
 	{
 		$conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/sql.sql", NULL, NULL); 
 	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
 
 		
