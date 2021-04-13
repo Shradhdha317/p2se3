a718e64d49d80235396f9bc22468d7348ab62608
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-12 23:53:42 -0400

    reverse

--- data.php
+++ data.php
@@ -7,7 +7,7 @@ function get_price($product)
    
 $conn=mysqli_init(); 
 	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
-	mysqli_real_connect($conn, "patels18@patels18@patels18.mysql.database.azure.com:3306", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
+	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
        $sql = "SELECT price FROM products WHERE product = '$product'";
 
        $result = $conn->query($sql);
