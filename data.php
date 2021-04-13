a5b599df5e8b4e1aeab4cd117fd571e32146e6bc
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-13 00:01:55 -0400

    ssl connection

--- data.php
+++ data.php
@@ -6,7 +6,7 @@ function get_price($product)
         
    
 $conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
 	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
        $sql = "SELECT price FROM products WHERE product = '$product'";
 
@@ -30,7 +30,7 @@ else {
 function get_designer($product)
 {
 $conn=mysqli_init(); 
-	mysqli_ssl_set($conn, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_ssl_set($conn, NULL, NULL, "C:\Users\shrad\Desktop\Github for cloud management\BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
 	mysqli_real_connect($conn, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
         
      	$sql1 = "SELECT designer FROM products WHERE product = '$product'";
